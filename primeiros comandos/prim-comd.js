3fb54531affbdd5b4a127daec51ec08f8056fffc
jefferson texeira <jtexeira078@gmail.com>
2025-07-17 21:44:50 -0300

    avancando em condicoes

--- primeiros comandos/prim-comd.js
+++ primeiros comandos/prim-comd.js
@@ -1,16 +1,2 @@
-//#region variaveis
-let nome = "jefferson";
-let idade = 18;
-//#endregion
-
-//#region funcoes
-function exibirMensagem() {
-    let nome = document.getElementById("nome").value;
-    let idade = document.getElementById("idade").value;
-    let mensagem = `Olá ${nome}, você tem ${idade} anos.`;
-    document.getElementById("mensagem").innerText = mensagem;
-}
-//#endregion funcoes
-
-
-exibirMensagem();
+console.log("Olá, Mundo!");
+console.log("Meu primeiro programa: trabalhando com variáveis");
